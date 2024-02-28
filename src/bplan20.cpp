9533ad1289644ac0ead207665d844316b2cc678e
Chanid-hong <155961674+Chanid-hong@users.noreply.github.com>
2024-02-28 13:03:11 +0100

    Update bplan20.cpp

--- src/bplan20.cpp
+++ src/bplan20.cpp
@@ -91,7 +91,7 @@ void BPlan20::run()
   {
     switch (state)
     { // make a shift in heading-mission
-      case 30: // Continue turn until right edge is almost reached, then follow right edge
+      case 30: // Following the edge
        
           toLog("Line detected, that is OK to follow");
           mixer.setEdgeMode(true /* left */, 0 /* offset */);
